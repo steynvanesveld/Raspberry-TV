664d45b71188e7c46826f89572e3cfe6b4f780eb
Steyn van Esveld <steyn.vanesveld@eijsink.nl>
2022-06-06 21:36:22 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+USERNAME="pipi"
+IP_ADDRESS="192.168.178.2"
+
+read -p "Compile the latest version? (y/n)" -n 1 -r COMPILE
+echo
+read -p "Deploy to server? (y/n)" -n 1 -r DEPLOY
+echo
+read -p "Reboot server? (y/n)" -n 1 -r REBOOT
+echo
+
+if [[ $COMPILE =~ ^[Yy]$ ]]
+then
+    npm run build
+fi
+
+if [[ $DEPLOY =~ ^[Yy]$ ]]
+then
+    scp -r dist/raspberry/ $USERNAME@$IP_ADDRESS:~/
+    ssh  -t $USERNAME@$IP_ADDRESS '
+        sudo rm -rf ~/lighttpd/*.js ~/lighttpd/*.txt ~/lighttpd/*.html ~/lighttpd/*.css ~/lighttpd/assets/
+        sudo mv -f ~/raspberry/* ~/lighttpd/
+        sudo rm -rf ~/raspberry
+    '
+fi
+
+if [[ $REBOOT =~ ^[Yy]$ ]]
+then
+  ssh  -t $USERNAME@$IP_ADDRESS 'sudo reboot'
+fi
+
