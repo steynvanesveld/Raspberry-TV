322075ffc8b21350139cb862f041b19a1dfad072
Steyn van Esveld <steyn.vanesveld@eijsink.nl>
2022-06-13 09:21:06 +0200

    Fix static wallpaper and music volume

--- src/app/modules/tv/components/tv-wallpaper/tv-wallpaper.component.ts
+++ src/app/modules/tv/components/tv-wallpaper/tv-wallpaper.component.ts
@@ -12,13 +12,14 @@ export class TvWallpaperComponent implements OnInit {
     public photoParameters = '?auto=compress&fit=crop&w=1920&h=1080';
     public gradient =
         'linear-gradient(to bottom, rgba(105, 67, 45, 0.75), rgba(0, 0, 0, 0.75))';
+    public dayIndex = 0;
     constructor(private pexelsService: PexelsService) {}
 
     get currentBackgroundImage() {
         let url = '';
 
         if (this.photos?.photos.length) {
-            const photo = this.photos.photos[0].src.original;
+            const photo = this.photos.photos[this.dayIndex].src.original;
             url = `url('${photo}${this.photoParameters}')`;
         }
 
@@ -27,11 +28,25 @@ export class TvWallpaperComponent implements OnInit {
 
     public ngOnInit(): void {
         this.getPhotos();
+        this.setCurrentDay();
     }
 
     private getPhotos(): void {
         this.pexelsService.getPhotos('Forest').subscribe((result) => {
             this.photos = result;
         });
+
+        setTimeout(() => {
+            this.getPhotos();
+        }, 1000 * 60 * 24 * 7); // 1 week
+    }
+
+    private setCurrentDay(): void {
+        const today = new Date();
+        this.dayIndex = today.getDate();
+
+        setTimeout(() => {
+            this.setCurrentDay();
+        }, 1000 * 60 * 6); // 6 hours
     }
 }
