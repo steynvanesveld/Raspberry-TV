98845ecd15f7a38d74413a97d7d39e65979c11f4
Steynno <steynno@gmail.com>
2025-02-17 16:23:51 +0100

    Use hash locations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { routes } from './app.routes';
-import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideZoneChangeDetection({ eventCoalescing: true }),
-        provideRouter(routes),
         provideHttpClient(),
+        provideRouter(routes, withHashLocation()),
+        provideZoneChangeDetection({ eventCoalescing: true }),
     ],
 };
